83a5b0dfaa3ce4da23003933c7d6f9225e6bc7ea
ndm <73387944+username1419@users.noreply.github.com>
2025-12-09 16:32:33 +1300

    refactor: simple builder pattern for `CinnamonGenericContainer`

--- src/app/inspector/base_classes/inspector_cinnamongenericcontainer.rs
+++ src/app/inspector/base_classes/inspector_cinnamongenericcontainer.rs
@@ -1,4 +1,4 @@
-use gtk::{glib, prelude::WidgetExt};
+use gtk::{Align, glib, prelude::WidgetExt};
 
 mod imp {
 
@@ -32,6 +32,38 @@ glib::wrapper! {
         @implements gtk::Buildable, gtk::ConstraintTarget, gtk::Accessible;
 }
 
+struct CinnamonGenericContainerBuilder {
+    valign: Align,
+    halign: Align,
+}
+
+impl Default for CinnamonGenericContainerBuilder {
+    fn default() -> Self {
+        Self {
+            valign: Align::Start,
+            halign: Align::Start,
+        }
+    }
+}
+
+impl CinnamonGenericContainerBuilder {
+    pub fn build(self) -> CinnamonGenericContainer {
+        glib::Object::builder::<CinnamonGenericContainer>()
+            .property("css-name", "CinnamonGenericContainer")
+            .property("valign", self.valign)
+            .property("halign", self.halign)
+            .build()
+    }
+
+    pub fn with_valign(&mut self, valign: Align) {
+        self.valign = valign;
+    }
+
+    pub fn with_halign(&mut self, halign: Align) {
+        self.halign = halign;
+    }
+}
+
 impl CinnamonGenericContainer {
     pub fn new(with_css_classes: &[&str]) -> Self {
         let this = glib::Object::builder::<CinnamonGenericContainer>()
@@ -40,5 +72,9 @@ impl CinnamonGenericContainer {
         this.set_css_classes(with_css_classes);
 
         this
+    }
+
+    pub fn builder() -> CinnamonGenericContainerBuilder {
+        CinnamonGenericContainerBuilder::default()
     }
 }
