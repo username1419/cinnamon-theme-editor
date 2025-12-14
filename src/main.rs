38cc68c81f61d140dcef06754b500a2553b797bb
ndm <73387944+username1419@users.noreply.github.com>
2025-12-14 16:23:18 +1300

    feat: minimal decorations implementation

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,27 @@
 use core::panic;
-
+use dioxus_desktop::{WindowBuilder, use_window, window};
 use simple_logger::SimpleLogger;
-
+use std::thread::Scope;
 pub mod app;
 pub mod helper;
-
-// The dioxus prelude contains a ton of common items used in dioxus apps. It's a good idea to import wherever you
-// need dioxus
-use dioxus::prelude::*;
-
-// We can import assets in dioxus with the `asset!` macro. This macro takes a path to an asset relative to the crate root.
-// The macro returns an `Asset` type that will display as the path to the asset in the browser or a local path in desktop bundles.
+use dioxus::{core::LaunchConfig, html::g::cx, prelude::*};
 const FAVICON: Asset = asset!("/assets/favicon.ico");
-// The asset macro also minifies some assets like CSS and JS to make bundled smaller
 const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
-
 fn main() {
     if cfg!(windows) {
         panic!("Unsupported on Windows");
     }
-
     match std::env::var("DESKTOP_SESSION") {
         Ok(value) => {
             if value != "cinnamon" {
                 panic!(
                     "This program is only compatible with the Cinnamon Desktop Environment. Current desktop environment: {}.",
-                    value
+                    value,
                 );
             }
         }
         Err(error) => panic!("{}", error),
     }
-
     let args = std::env::args().skip(1);
     let mut unknown_args = Vec::new();
     for arg in args {
@@ -45,28 +35,61 @@ fn main() {
             }
         }
     }
-
-    // The `launch` function is the main entry point for a dioxus app. It takes a component and renders it with the platform feature
-    // you have enabled
-    dioxus::launch(App);
+    dioxus::LaunchBuilder::desktop()
+        .with_cfg(
+            dioxus_desktop::Config::new().with_window(
+                WindowBuilder::new()
+                    .with_decorations(false)
+                    .with_resizable(true)
+                    .with_transparent(true),
+            ),
+        )
+        .launch(App);
 }
-
 /// App is the main component of our app. Components are the building blocks of dioxus apps. Each component is a function
 /// that takes some props and returns an Element. In this case, App takes no props because it is the root of our app.
 ///
 /// Components should be annotated with `#[component]` to support props, better error messages, and autocomplete
 #[component]
 fn App() -> Element {
-    // The `rsx!` macro lets us define HTML inside of rust. It expands to an Element with all of our HTML inside.
     rsx! {
-        // In addition to element and text (which we will see later), rsx can contain other components. In this case,
-        // we are using the `document::Link` component to add a link to our favicon and main CSS file into the head of our app.
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS }
+        div {
 
-        p {
-            "lorem ispum"
+        class: "window",
+            div { class: "titlebar", "data-tauri-drag-region": "true",
+                div {
+            class: "titlebar-left",
         }
+        div {
+        class: "titlebar-center",
 
+                span { class: "title", "Cinnamon Theme Editor" }}
+        div {
+            class: "titlebar-right",
+                div { class: "window-controls",
+                    button {
+                        class: "wcontrol-minimize",
+                        onclick: move |_| {
+                            use_window().set_minimized(true);
+                        },
+                    }
+                    button {
+                        class: "wcontrol-maxmize",
+                        onclick: move |_| {
+                            let w = use_window();
+                            w.set_maximized(!w.is_maximized());
+                        },
+                    }
+                    button {
+                        class: "wcontrol-exit",
+                        onclick: move |_| {
+                            use_window().close();
+                        },
+                    }
+                }
+            }
+        }}
     }
 }
