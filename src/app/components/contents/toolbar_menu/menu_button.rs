131aef89547f17cb6ff931a48a5e44e606308905
ndm <73387944+username1419@users.noreply.github.com>
2025-12-19 05:59:43 +1300

    feat: shortcut display on toolbar menu button

--- src/app/components/contents/toolbar_menu/menu_button.rs
+++ src/app/components/contents/toolbar_menu/menu_button.rs
@@ -1,4 +1,5 @@
 use std::rc::Rc;
+use std::str::FromStr;
 
 use dioxus::html::geometry::Coordinates;
 use dioxus::html::geometry::euclid::Point2D;
@@ -41,6 +42,26 @@ pub struct MenuButtonProps {
 #[component]
 pub fn MenuButton(props: MenuButtonProps) -> Element {
     let text = use_hook(|| props.text.unwrap_or_default());
+    let shortcut_label = use_hook(|| {
+        props
+            .shortcut
+            .clone()
+            .map(|shortcut| {
+                let mut label = String::new();
+                label.push_str(shortcut.key.to_string().as_str());
+                if shortcut.shift {
+                    label = format!("Shift+{}", label);
+                }
+                if shortcut.alt {
+                    label = format!("Alt+{}", label);
+                }
+                if shortcut.ctrl {
+                    label = format!("Ctrl+{}", label);
+                }
+                label
+            })
+            .unwrap_or_default()
+    });
 
     rsx! {
         button {
@@ -55,6 +76,8 @@ pub fn MenuButton(props: MenuButtonProps) -> Element {
                     let match_alt = event.modifiers().alt() == shortcut.alt;
 
                     // tower of doom? more like mountain of doom LMAO
+                    // NOTE: actually in hindsight it would be better if we get the cursor
+                    // position instead
                     if match_key && match_alt && match_shift && match_ctrl {
                         props
                             .onclick
@@ -81,6 +104,7 @@ pub fn MenuButton(props: MenuButtonProps) -> Element {
                 }
             },
             "{text}"
+            span { class: "shortcut-text", "{shortcut_label}" }
         }
     }
 }
