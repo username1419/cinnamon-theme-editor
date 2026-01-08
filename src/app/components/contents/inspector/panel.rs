6686cc7daf38c4a7f7df0f534082a288f78ba623
ndm <73387944+username1419@users.noreply.github.com>
2026-01-08 17:50:47 +0700

    feat: panel styling

--- src/app/components/contents/inspector/panel.rs
+++ src/app/components/contents/inspector/panel.rs
@@ -6,6 +6,7 @@ use dioxus::{
 
 use crate::app::components::contents::inspector::common::cinnamongenericcontainer::CinnamonGenericContainer;
 use crate::app::components::contents::inspector::common::stboxlayout::{Orientation, StBoxLayout};
+use crate::app::dconf;
 use crate::app::io::parser::selector::SelectorCategory;
 use crate::config::AppConfiguration;
 
@@ -13,12 +14,13 @@ use crate::config::AppConfiguration;
 pub fn Panel() -> Element {
     let config = use_context::<AppConfiguration>();
     let selected_category = config.inspector_type;
+    let panel_size = use_hook(|| 24);
     rsx! {
         // NOTE: we gotta treat certain widget names as classes for this to work
         div {
             class: "stage inspector-content",
             style: if selected_category.read().ne(&SelectorCategory::Panel) { "display: none" },
-            CinnamonGenericContainer { class: "panel-top",
+            CinnamonGenericContainer { class: "panel-top", style: "width: {panel_size}px",
                 StBoxLayout { class: "panelLeft", orientation: Orientation::Horizontal,
                     div {}
                 }
@@ -31,7 +33,7 @@ pub fn Panel() -> Element {
                     div {}
                 }
             }
-            CinnamonGenericContainer { class: "panel-left",
+            CinnamonGenericContainer { class: "panel-left", style: "height: {panel_size}px",
                 StBoxLayout { class: "panelLeft", orientation: Orientation::Vertical,
                     div {}
                 }
@@ -42,7 +44,7 @@ pub fn Panel() -> Element {
                     div {}
                 }
             }
-            CinnamonGenericContainer { class: "panel-right",
+            CinnamonGenericContainer { class: "panel-right", style: "height: {panel_size}px",
                 StBoxLayout { class: "panelLeft", orientation: Orientation::Vertical,
                     div {}
                 }
@@ -53,7 +55,7 @@ pub fn Panel() -> Element {
                     div {}
                 }
             }
-            CinnamonGenericContainer { class: "panel-bottom",
+            CinnamonGenericContainer { class: "panel-bottom", style: "width: {panel_size}px",
                 StBoxLayout { class: "panelLeft", orientation: Orientation::Horizontal,
                     div {}
                 }
