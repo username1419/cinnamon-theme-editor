3f554721abb7e9b3fb68b0fd20094f8df17bb58d
ndm <73387944+username1419@users.noreply.github.com>
2026-02-13 01:43:43 +1300

    fix: fixes weird cursor teleport

--- src/app/components/contents/property_config/color_picker.rs
+++ src/app/components/contents/property_config/color_picker.rs
@@ -1,3 +1,5 @@
+use dioxus::html::geometry::euclid::Point2D;
+use dioxus::html::geometry::{ClientSpace, ElementSpace};
 use dioxus::html::input_data::MouseButton;
 use dioxus::prelude::*;
 use dioxus::{
@@ -10,12 +12,11 @@ use crate::app::io::parser::property::Property;
 use crate::app::io::parser::property_value::Value;
 use crate::config::AppConfiguration;
 
-const CURSOR_SIZE: f64 = 8.0;
-
 #[component]
 pub fn ColorPicker() -> Element {
     // TODO: set default as original element color somehow
     let config = use_context::<AppConfiguration>();
+    let mouse_state = config.mouse_state;
     let mut editing_style = config.element_style;
     let mut selected_hue = use_signal(|| 0 as u32);
     let mut selected_saturation = use_signal(|| 0);
@@ -23,11 +24,14 @@ pub fn ColorPicker() -> Element {
     let mut selected_alpha = use_signal(|| 100);
 
     let mut saturation_lightness_select_rect = use_signal(|| (0.0, 0.0));
-    let mut cursor_pos = use_signal(|| (0.0, 0.0));
+    let mut cursor_pos: Signal<Point2D<f64, ElementSpace>> = use_signal(|| Point2D::origin());
     let mut history = use_signal(|| [(0 as u32, 100 as i32, 100 as i32, 100 as i32); 10]);
 
-    const REFRESH_RATE: f64 = 1.0 / 60.0;
+    let refresh_rate = time::Duration::from_secs_f64(1.0 / 60.0);
+    let refresh_rate_slow = time::Duration::from_secs_f64(1.0 / 10.0);
     let mut last_time = use_signal(|| Instant::now());
+    let mut last_time_slow = use_signal(|| Instant::now());
+    let mut offset: Signal<Point2D<f64, ClientSpace>> = use_signal(|| Point2D::origin());
 
     rsx! {
         div { class: "color-picker",
@@ -45,45 +49,49 @@ pub fn ColorPicker() -> Element {
                         // ~4% CPU (Intel i5-13420H)
                         // ~7% GPU (GeForce RTX 4050 Mobile)
                         async move {
-                            if !config.mouse_state.read().mouse_down.contains(MouseButton::Primary) {
+                            if !config.mouse_state.peek().mouse_down.contains(MouseButton::Primary) {
                                 return;
                             }
 
                             // NOTE: reducing refresh rate on my machine only decrases cpu/gpu
-                            // usage by 0.5% at most
+                            // usage by between 0-0.5%
                             let delta = time::Instant::now().duration_since(*last_time.peek());
-                            if delta.as_secs_f64() < REFRESH_RATE {
+                            if delta < refresh_rate {
                                 return;
                             }
 
-                            let bounds = *saturation_lightness_select_rect.read();
+                            let bounds = *saturation_lightness_select_rect.peek();
 
-                            let relative_coord = event.element_coordinates();
-                            let absolute_coord = event.client_coordinates();
-                            // offset of bounding box from top left of viewport
-                            let offset_x = absolute_coord.x - relative_coord.x;
-                            let offset_y = absolute_coord.y - relative_coord.y;
+                            let global_coord = mouse_state.peek().coordinates.client();
+                            if offset.peek().eq(&Point2D::origin()) {
+                                let relative_coord = event.element_coordinates();
+                                *offset.write() = Point2D::new(global_coord.x - relative_coord.x, global_coord.y - relative_coord.y);
+                            }
+                            let offset = offset.peek();
+                            let relative_coord: Point2D<f64, ElementSpace> = Point2D::new(global_coord.x - offset.x, global_coord.y - offset.y);
+                            *cursor_pos.write() = relative_coord.clone();
 
-                            // subtract cursor size so it looks good
-                            let raw_x: f64 = relative_coord.x - CURSOR_SIZE;
-                            let raw_y: f64 = relative_coord.y - CURSOR_SIZE;
+                            // NOTE: changing this refresh rate doesnt really improve anything
+                            // either, its probably something ive fucked up in auto reactivity
+                            *last_time.write() = time::Instant::now();
+                            let delta_s = time::Instant::now().duration_since(*last_time_slow.peek());
+                            if delta_s < refresh_rate_slow {
+                                return;
+                            }
 
                             // normalize cursor position relative to bounding box
-                            // i have a sneaking suspicion that this is the problem but i got
-                            // nothin
-                            let normalized_x = (raw_x / bounds.0).clamp(0.0, 1.0);
-                            let normalized_y = (raw_y / bounds.1).clamp(0.0, 1.0);
-
-                            let cursor_x = offset_x + normalized_x * bounds.0;
-                            let cursor_y = offset_y + normalized_y * bounds.1;
-
-                            cursor_pos.set((cursor_x, cursor_y));
+                            let normalized_x = (relative_coord.x / bounds.0).clamp(0.0, 1.0);
+                            let normalized_y = (relative_coord.y / bounds.1).clamp(0.0, 1.0);
 
                             let saturation = normalized_x;
                             let value = 1.0 - normalized_y;
                             // conversion to hsl bc im stupid
                             let lightness = ((value * (1.0 - saturation / 2.0)) * 100.0) as u32;
                             let saturation_percent = (saturation * 100.0) as u32;
+                            if lightness > 90 {
+                                debug!("{:?}", relative_coord);
+                                debug!("{} {}", normalized_x, normalized_y);
+                            }
 
                             *selected_lightness.write() = lightness;
                             *selected_saturation.write() = saturation_percent;
@@ -99,18 +107,18 @@ pub fn ColorPicker() -> Element {
                             editing_style
                                 .write()
                                 .set_style_attribute(Property::from_raw("background-color"), values);
-                            *last_time.write() = time::Instant::now();
+                            *last_time_slow.write() = time::Instant::now();
                         }
                     },
+                    div {
+                        class: "saturation-brightness-cursor",
+                        style: r#"left: {cursor_pos().x}px; top: {cursor_pos().y}px;background-color: hsl({selected_hue}, {selected_saturation}%, {selected_lightness}%);"#,
+                    }
                 }
                 div {
                     class: "color-preview",
                     style: "background-color: hsla({selected_hue}, {selected_saturation}%, {selected_lightness}%, {selected_alpha}%);",
                 }
-            }
-            div {
-                class: "saturation-brightness-cursor",
-                style: r#"left: {cursor_pos.read().0}px; top: {cursor_pos.read().1}px;background-color: hsl({selected_hue}, {selected_saturation}%, {selected_lightness}%);"#,
             }
             input {
                 // NOTE: i wanna show the selected hue on thumb but that would require
