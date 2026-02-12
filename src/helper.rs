c2cc405f7e0542bfc4863a5bbb6f54493d48e22c
ndm <73387944+username1419@users.noreply.github.com>
2026-02-13 02:06:27 +1300

    refactor: remove unused function

--- src/helper.rs
+++ src/helper.rs
@@ -1,8 +1,6 @@
 use dioxus::html::geometry::{Coordinates, euclid::Point2D};
 use dioxus_desktop::wry::dpi::PhysicalPosition;
 
-use crate::app::io::parser::selector::SelectorCategory;
-
 pub struct Helper; // idk why im doing this but it feels like its right to my oop addicted brain
 impl Helper {
     pub fn to_coord(pos: PhysicalPosition<f64>) -> Coordinates {
@@ -12,15 +10,5 @@ impl Helper {
         let page = Point2D::new(pos.x, pos.y);
 
         Coordinates::new(screen, client, element, page)
-    }
-
-    pub fn get_heirarchy_key(class: &String) -> SelectorCategory {
-        let class = class.to_lowercase();
-
-        if class.contains("panel") {
-            return SelectorCategory::Panel;
-        }
-
-        SelectorCategory::Other
     }
 }
