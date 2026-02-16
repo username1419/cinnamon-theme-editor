08f59498f009868adca48e4cff6cdf4c01cac0ca
ndm <73387944+username1419@users.noreply.github.com>
2026-02-16 14:58:29 +1300

    refactor: panels_height code

--- src/app/components/contents/inspector/panel.rs
+++ src/app/components/contents/inspector/panel.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
+
 use dioxus::prelude::*;
 use dioxus::{
     core::Element,
     prelude::{component, rsx},
 };
+use once_cell::sync::Lazy;
 
 use crate::app::components::contents::inspector::common::cinnamongenericcontainer::CinnamonGenericContainer;
 use crate::app::components::contents::inspector::common::stboxlayout::{Orientation, StBoxLayout};
@@ -10,46 +13,44 @@ use crate::app::dconf;
 use crate::app::io::parser::selector::SelectorCategory;
 use crate::config::AppConfiguration;
 
+static PANEL_PROPERTIES: Lazy<Vec<(u8, u8, String)>> = Lazy::new(|| {
+    dconf::CinnamonSettings::get_enabled_panels()
+        .inspect_err(|e| error!("Error while retrieving panel properties: {}", e))
+        .unwrap_or_default()
+});
+
+static PANELS_HEIGHT: Lazy<Vec<(u8, u8)>> = Lazy::new(|| {
+    dconf::CinnamonSettings::get_panels_height()
+        .inspect_err(|e| error!("Error while retreiving panels height: {}", e))
+        .unwrap_or_default()
+});
+
 #[component]
 pub fn Panel() -> Element {
     let config = use_context::<AppConfiguration>();
     let selected_category = config.inspector_type;
-    let panel_properties = dconf::CinnamonSettings::get_enabled_panels()
-        .inspect_err(|e| error!("Error while retrieving panel properties: {}", e))
-        .unwrap_or_default();
-    let panels_height = dconf::CinnamonSettings::get_panels_height()
-        .inspect_err(|e| error!("Error while retreiving panels height: {}", e))
-        .unwrap_or_default();
     let panels_height = use_hook(|| {
-        // [top, left, right, bottom]
-        let mut out = [0 as u8; 4];
-        // PERF: this is o(n*m) but the size of this isnt that big so its probably fine
-        // also i cant think of any other way to do this (theres likely a better way to)
-        panel_properties
-            .into_iter()
-            .filter(|v| v.1 == 0)
-            .for_each(|t| {
-                let size = panels_height
-                    .iter()
-                    .find_map(|t1| if t1.0 == t.0 { Some(t1.1) } else { None })
-                    .unwrap_or(0);
-                match t.2.as_str() {
-                    "top" => out[0] = size,
-                    "left" => out[1] = size,
-                    "right" => out[2] = size,
-                    "bottom" => out[3] = size,
-                    _ => (),
+        PANEL_PROPERTIES
+            .iter()
+            .filter_map(|(panel_id, monitor_id, position)| {
+                if 0.eq(monitor_id) {
+                    return None;
                 }
-            });
 
-        out
+                let height = PANELS_HEIGHT
+                    .iter()
+                    .find(|(panel_id2, _)| panel_id == panel_id2)
+                    .unwrap_or(&(0, 0))
+                    .1;
+                Some((position.clone(), height))
+            })
+            .collect::<HashMap<String, u8>>()
     });
     rsx! {
-        // NOTE: we gotta treat certain widget names as classes for this to work
         div {
             class: "stage inspector-content",
             style: if selected_category.read().ne(&SelectorCategory::Panel) { "display: none" },
-            CinnamonGenericContainer { class: "panel-top", style: "height: {panels_height[0]}px",
+            CinnamonGenericContainer { class: "panel-top", style: "height: {panels_height[\"top\"]}px",
                 StBoxLayout { class: "panelLeft", orientation: Orientation::Horizontal,
                     div {}
                 }
@@ -62,7 +63,7 @@ pub fn Panel() -> Element {
                     div {}
                 }
             }
-            CinnamonGenericContainer { class: "panel-left", style: "width: {panels_height[1]}px",
+            CinnamonGenericContainer { class: "panel-left", style: "width: {panels_height[\"left\"]}px",
                 StBoxLayout { class: "panelLeft", orientation: Orientation::Vertical,
                     div {}
                 }
@@ -73,7 +74,7 @@ pub fn Panel() -> Element {
                     div {}
                 }
             }
-            CinnamonGenericContainer { class: "panel-right", style: "width: {panels_height[2]}px",
+            CinnamonGenericContainer { class: "panel-right", style: "width: {panels_height[\"right\"]}px",
                 StBoxLayout { class: "panelLeft", orientation: Orientation::Vertical,
                     div {}
                 }
@@ -84,7 +85,7 @@ pub fn Panel() -> Element {
                     div {}
                 }
             }
-            CinnamonGenericContainer { class: "panel-bottom", style: "height: {panels_height[3]}px",
+            CinnamonGenericContainer { class: "panel-bottom", style: "height: {panels_height[\"bottom\"]}px",
                 StBoxLayout { class: "panelLeft", orientation: Orientation::Horizontal,
                     div {}
                 }
