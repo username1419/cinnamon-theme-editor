dad0ebcd242dd3ca1439fab6635b9603c16c0f00
ndm <73387944+username1419@users.noreply.github.com>
2026-05-26 23:54:58 +1200

    feat: animate `EditorSection`

--- src/app/components/contents/property_editor/editor_section.rs
+++ src/app/components/contents/property_editor/editor_section.rs
@@ -1,16 +1,26 @@
+use std::time::Duration;
+
 use dioxus::{core::Element, prelude::*};
+use tokio::time;
 
 #[component]
 pub fn EditorSection(class: String, label: String, children: Element) -> Element {
-    let mut style = use_signal(String::new);
+    let mut style = use_signal(|| String::from("display: none"));
+    let mut height_style = use_signal(move || format!("max-height: {}%", 0));
+    let mut opened = use_signal(String::new);
 
     rsx! {
         div {
             class: "collapsible-container",
-            onclick: move |_| {
+            onclick: move |_| async move {
                 if &style() == "display: none" {
                     style.set("display: flex".to_string());
+                    opened.set("open".to_string());
+                    height_style.set(format!("max-height: {}%", 100));
                 } else {
+                    opened.set(String::new());
+                    height_style.set(format!("max-height: {}%", 0));
+                    time::sleep(Duration::from_secs_f64(0.2)).await;
                     style.set("display: none".to_string());
                 }
             },
@@ -20,10 +30,15 @@ pub fn EditorSection(class: String, label: String, children: Element) -> Element
                 span { style: "right: 0%; margin-left: auto", if &style() == "display: none" { "+" } else { "-" } },
             }
             div {
-                class: "collapsible {class}",
-                style: "{style}",
-                onclick: move |evt| evt.stop_propagation(),
-                {children}
+                class: "collapsible-wrapper {opened}",
+                div {
+                    class: "collapsible {class}",
+                    style: "{height_style}; {style}",
+                    onclick: move |evt| {
+                        evt.stop_propagation();
+                    },
+                    {children}
+                }
             }
         }
     }
